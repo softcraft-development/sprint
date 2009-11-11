c806673e10a09e65b183491d7ca20c71d86377d3
Craig Walker <github@softcraft.ca>
2009-11-11 12:23:14 -0700

    Added basic functionality

--- lib/sprint.rb
+++ lib/sprint.rb
@@ -2,5 +2,40 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Sprint
-  VERSION = '0.0.1'
+  VERSION = '1.0.0'
+  class Status
+    def initialize( output, status )
+      @output = output
+      @status = status
+    end
+    
+    def to_s
+      @output
+    end
+    alias :output :to_s
+    alias :to_string :to_s
+    
+    def to_i
+      @status.exitstatus
+    end
+    alias :return_code :to_i
+    alias :to_int :to_i
+    
+    def success?
+      @status.success?
+    end
+    alias :to_b :success?
+    
+    def self.run( command )
+      output = `#{command}`
+      status = $?
+      Status.new output, status
+    end
+  end
+end
+
+module Kernel
+  def sprint( command )
+    Sprint::Status.run command
+  end
 end
--- test/test_sprint.rb
+++ test/test_sprint.rb
@@ -1,11 +1,28 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
+require 'shoulda'
 
 class TestSprint < Test::Unit::TestCase
 
   def setup
   end
   
-  def test_truth
-    assert true
+  context "The run command" do
+    context "when executing 'echo Test'" do
+      setup do
+        @results = sprint "echo Test"
+      end
+
+      should "return the word 'Test' as its execution results" do
+        assert_equal "Test", @results.output.strip
+      end
+
+      should "indicate success" do
+        assert_equal true, @results.success?
+      end
+
+      should "have a zero status" do
+        assert_equal 0, @results.return_code
+      end
+    end
   end
 end
